5fc488c708e8cf3e32085ac82cf75d30a9febebb
Reinard Pieters <reinardpiet@gmail.com>
2025-05-09 19:07:24 +0200

    Fixed the file format to the one they gave

---
+++ app.js
@@ -0,0 +1,21 @@
+// app.js
+const express = require("express");
+const path = require("path");
+const pageRoutes = require("./routes/pageRoutes");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", pageRoutes);
+
+app.listen(port, host, () => {
+  console.log(`Server running on http://${host}:${port}`);
+});
