86d429da2499140ce61ada5b81e3f63ef478ddb0
Reinard Pieters <reinardpiet@gmail.com>
2025-05-10 15:32:53 +0200

    Added the events part to generate the events

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,6 +3,34 @@ const express = require('express');
 const router = express.Router();
 const Contact = require("../models/Contact");
 
+const contacts = []
+const events = [
+  {
+    name: "Community Meetup",
+    date: new Date('2025-06-15'),
+    location: "Community Center, Main St.",
+    image: "/images/event1.jpg"
+  },
+  {
+    name: "Summer BBQ",
+    date: new Date('2025-07-10'),
+    location: "Park Side, Elm Street",
+    image: "/images/event2.jpg"
+  },
+  {
+    name: "Charity Run",
+    date: new Date('2025-08-05'),
+    location: "Downtown Square",
+    image: "/images/event3.jpg"
+  },
+  {
+    name: "Art Exhibition",
+    date: new Date('2025-09-20'),
+    location: "City Gallery, 5th Ave.",
+    image: "/images/event4.jpg"
+  }
+];
+
 router.get('/', (req, res) => {
     res.render("pages/home")
 });
@@ -12,7 +40,7 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/events', (req, res) => {
-    res.render("pages/events")
+    res.render("pages/events", {events})
 });
 
 router.get('/contact', (req, res) => {
@@ -29,6 +57,8 @@ router.post('/contact', (req, res) => {
     });
     newContact.save()
         .then(() => {
+            contacts.push(newContact);
+            console.log("Contact saved:", newContact);
             console.log("Contact saved successfully");
             res.redirect('/thankyou');
         })
