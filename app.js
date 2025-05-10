e58d059a6ae53c1792046ab81ab3fbebce52df4a
Reinard Pieters <reinardpiet@gmail.com>
2025-05-10 13:06:22 +0200

    Updated the git ignore to ignore the modules and .env file as well as set up all the routing and basic post for the contact page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT;
 const host = process.env.HOST;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,18 +4,30 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.render("pages/home")
 });
 
 router.get('/about', (req, res) => {
+    res.render("pages/about")
 });
 
 router.get('/events', (req, res) => {
+    res.render("pages/events")
 });
 
 router.get('/contact', (req, res) => {
+    res.render("pages/contact")
 });
 
+router.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    
+    res.redirect('/thankyou');
+});
 router.get('/thankyou', (req, res) => {
+    res.render("pages/thankyou")
 });
 
 module.exports = router;
